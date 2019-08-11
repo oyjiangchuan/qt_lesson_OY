264b84f3a3f8ab473e0bfe539dc9b2da3f43a291
Loserchuan <1366325462@qq.com>
2019-08-11 22:36:38 +0800

    第一个微信小程序

--- wx-app/shopping/pages/user/user.js
+++ wx-app/shopping/pages/user/user.js
@@ -8,11 +8,40 @@ Page({
 
   },
 
+  payOrders() {
+    wx.requestPayment({ // 发起微信支付
+      timeStamp: 'String1', //时间戳，从 1970 年 1 月 1 日 00:00:00 至今的秒数，即当前的时间
+      nonceStr: 'String2', // 随机字符串，长度为32个字符以下
+      package: 'String3', // 统一下单接口返回的 prepay_id 参数值，提交格式如：prepay_id=***
+      signType: 'MD5', // 签名算法
+      paySign: 'String4', // 签名，具体签名方案参见
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function (res) {
+        wx.showModal({ // 显示模态对话框
+          title: '支付提示',
+          content: '<text>',
+          showCancel: false
+        })
+      }
+    })
+  },
+
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let self = this
+    wx.request({
+      url: 'http://www.gdfengshuo.com/api/wx/orders.txt',
+      success(res) {
+        self.setData({
+          orders: res.data
+        })
+      }
+    })
   },
 
   /**
@@ -26,7 +55,16 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    wx.getStorage({ // 从本地缓存中异步获取指定key的内容
+      key: 'address',
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          hasAddress: true,
+          address: res.data
+        })
+      }
+    })
   },
 
   /**
