19c0c6025f7342e08ab08f4e2dfc027248966e8b
Loserchuan <1366325462@qq.com>
2019-12-02 22:34:27 +0800

    TS+Vue3.0开发个人博客

---
+++ TS/blog/myblog/README.md
@@ -0,0 +1,1 @@
+- npm i element-ui -s
---
+++ TS/blog/myblog/src/components/RegisterAndLogin.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-dialog title="登录" :visible.sync="dialogVisible" @close="cancel">
+    <el-form label-width="80px">
+      <el-form-item label="邮箱">
+        <el-input v-model="params.emil"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="params.password"></el-input>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component, Emit } from "vue-property-decorator";
+@Component // 一定要添加 否则无法正常使用get(计算属性)
+export default class RegisterAndLogin extends Vue {
+  @Prop({ default: false }) visible!: boolean; // Prop默认的写法
+  private params: any = {
+    emil: "",
+    password: ""
+  }
+  get dialogVisible(): boolean {
+    // console.log(this.visible)
+    return this.visible;
+  }
+  @Emit() // emit的方式
+  private cancel(): boolean {
+    return false;
+  }
+}
+</script>
+
+<style>
+</style>
